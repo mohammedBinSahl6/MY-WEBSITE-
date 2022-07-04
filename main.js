7d2f3807460bf9d4a72b19f9f98a81f71dcc9035
mohammed dbs <mohammed.dbs.660@gmail.com>
2022-07-04 22:20:46 +0200

    version 1.5

--- main.js
+++ main.js
@@ -30,4 +30,17 @@ $('.btn3').click(function(){
 
 
 })
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+          "body").style.visibility = "hidden";
+        document.querySelector(
+          "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+          "#loader").style.display = "none";
+        document.querySelector(
+          "body").style.visibility = "visible";
+    }
+};
 
